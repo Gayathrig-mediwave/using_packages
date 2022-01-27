70ffe098af52c07007df2ca6f53d0522be1dfb53
Gayathrig-mediwave <gayathridevi_g@mindwaveventures.com>
2022-01-27 16:11:18 +0530

    after creating a http server

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const moment = require("moment");
+const http = require("http");
+const port = 8080;
 const date = moment().format("DD / MM / YYYY");
 console.log(date);
 const future = moment().add(3, "days");
 console.log(future.format("DD / MM / YYYY"));
+function logger(req) {
+  console.log(`${new Date()} - ${req.url} : ${req.method}`);
+}
+
+http
+  .createServer(function (req, res) {
+    logger(req);
+    const html = "<h1>Hey Guys</h1>";
+    const json = {
+      name: "hedge",
+      id: 1010,
+    };
+    //res.setHeader("content-type", "text/html");
+    //res.write(html);
+    res.setHeader("content-type", "application/json");
+    res.write(JSON.stringify(json));
+    res.end();
+  })
+  .listen(port);
